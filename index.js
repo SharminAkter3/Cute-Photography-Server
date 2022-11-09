538e5cee1f00e6b647e400978d773c6bd00e5ddc
SharminAkter3 <mssharmin4145@gmail.com>
2022-11-08 23:15:14 -0800

    added reviews api

--- index.js
+++ index.js
@@ -24,19 +24,28 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollection = client.db('cutePhoto').collection('services');
+        const reviewCollection = client.db('cutePhoto').collection('reviews')
 
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
-        })
+        });
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
             res.send(service)
+        });
+
+        //review api 
+
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
 
 
